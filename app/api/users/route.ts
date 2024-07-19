0f2e3d448528f07531cf2107a1e9290b4b138cf1
NaNguyen <yu19151998@gmail.com>
2024-07-19 16:55:48 +0700

    feat(users-api): handle users endpoints

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,7 +6,7 @@ import { regexNoSpecialCharsOrSpaces } from "@/app/utils";
 import Register from "../models/Register";
 
 const registerUserSchema = z.object({
-  token: z.string().min(1),
+  email: z.string().min(1).email(),
   username: z.string().regex(regexNoSpecialCharsOrSpaces).optional(),
   account: z.string().regex(regexNoSpecialCharsOrSpaces).optional(),
   databaseName: z.string().regex(regexNoSpecialCharsOrSpaces).optional(),
@@ -27,20 +27,23 @@ export async function POST(req: NextRequest) {
     );
 
   try {
-    const existingRegister = await Register.findById(body.token);
-    if (existingRegister) {
-      if (!!body.username) {
-        existingRegister.username = body.username;
-      }
-      if (!!body.account) {
-        existingRegister.account = body.account;
-      }
-      if (!!body.databaseName) {
-        existingRegister.databaseName = body.databaseName;
-      }
-      await existingRegister.save();
-      return Response.json(existingRegister, { status: 200 });
+    const existingRegister = await Register.findOne({ email: body.email });
+    if (!existingRegister)
+      return Response.json(
+        { ...body, error: t("validationMessage.form.invalid") },
+        { status: 404 }
+      );
+    if (!!body.username) {
+      existingRegister.username = body.username;
     }
+    if (!!body.account) {
+      existingRegister.account = body.account;
+    }
+    if (!!body.databaseName) {
+      existingRegister.databaseName = body.databaseName;
+    }
+    await existingRegister.save();
+    return Response.json(existingRegister, { status: 200 });
   } catch (err: any) {
     return Response.json({ ...body, error: err.message }, { status: 400 });
   }
@@ -58,11 +61,45 @@ export async function GET(req: NextRequest) {
   const locale = searchParams.get("locale");
   const t = await getTranslations({ locale });
 
-  const body = await req.json();
-  const validation = checkTokenSchema.safeParse(body);
+  const token = searchParams.get("token");
+  const email = searchParams.get("email");
+  const expiryDate = searchParams.get("expiryDate");
+
+  const validation = checkTokenSchema.safeParse({ token, email, expiryDate });
   if (!validation.success)
     return Response.json(
-      { ...body, error: t("validationMessage.form.invalid") },
+      { error: t("validationMessage.form.invalid") },
       { status: 400 }
     );
+
+  try {
+    const existingRegister = await Register.findOne({
+      email: validation.data.email,
+    });
+
+    if (!existingRegister) {
+      return Response.json(
+        { error: t("validationMessage.form.invalid") },
+        { status: 404 }
+      );
+    }
+
+    if (
+      existingRegister.token !== validation.data.token ||
+      existingRegister.expiryDate !== validation.data.expiryDate
+    ) {
+      return Response.json(
+        { error: t("validationMessage.form.invalid") },
+        { status: 400 }
+      );
+    }
+
+    if (new Date().valueOf() > existingRegister.expiryDate) {
+      return Response.json({ error: t("error.tokenExpired") }, { status: 400 });
+    }
+
+    return Response.json(existingRegister, { status: 200 });
+  } catch (err: any) {
+    return Response.json({ error: err.message }, { status: 400 });
+  }
 }
